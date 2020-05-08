192ecf55d6ab2c0893b7682dbdb6522d53ec2c94
LFZ96 <loganzipkes@gmail.com>
2020-05-08 17:13:51 -0500

    Added favicon

--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -4,7 +4,7 @@ const Pagination = props => {
   const pageLinks = [];
 
   for(let i = 1; i < props.pages + 1; i++) {
-    let active = props.currentPage == i ? 'active' : '';
+    let active = props.currentPage === i ? 'active' : '';
     
     pageLinks.push(<li className={`page-item ${active}`} key={i} onClick={() => props.updatePage(i)}><a className="page-link" href="#">{i}</a></li>);
   }
