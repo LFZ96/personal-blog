a0c4e8d62c59d1cf852fec5565964a55c8c8d89f
LFZ96 <loganzipkes@gmail.com>
2020-05-19 18:07:59 -0500

    added catch all for react router

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use('/auth', authRouter);
 app.use('/api', apiRouter);
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
@@ -57,7 +57,7 @@ app.get('/*', function(req, res) {
 })
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 }
 
 const PORT = process.env.PORT || 5000;
