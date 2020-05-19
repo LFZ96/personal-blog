42e94d462d8df35bada6e53cc0d8dd7a22af7967
LFZ96 <loganzipkes@gmail.com>
2020-05-19 15:46:17 -0500

    added catch all for react router

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use('/auth', authRouter);
 app.use('/api', apiRouter);
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
