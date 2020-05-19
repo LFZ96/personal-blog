7b6aaaab27fb9387d2ef113b8e816eabbd0150bd
LFZ96 <loganzipkes@gmail.com>
2020-05-19 16:59:35 -0500

    added catch all for react router

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.use('/api', apiRouter);
 // })
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build/index.html')));
 }
 
 const PORT = process.env.PORT || 5000;
